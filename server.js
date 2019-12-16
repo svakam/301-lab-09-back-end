c0766df0530d1972f5ed0fa122457d471cefb434
Vik Akam <vikramakam@hotmail.com>
2019-12-15 19:22:03 -0800

    test git remote set-url origin

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ let saveToDatabase = (locationOb) => {
   let sql = 'INSERT INTO city_explorer (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4)';
   let safeValues = [locationOb.search_query, locationOb.formatted_query, locationOb.latitude, locationOb.longitude];
   client.query(sql, safeValues);
-}
+};
 
 let dailyWeather = (city, response) => {
   let latitude = city.latitude;
